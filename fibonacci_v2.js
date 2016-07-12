ba054f9ec5275743dbbb8f322e15404f9d172710
Christoph Anderson <cwolfanderson@gmail.com>
2016-07-12 12:28:17 -0700

    Finished fibonacci

---
+++ fibonacci_v2.js
@@ -0,0 +1,23 @@
+/*
+Compute the nth Fibonacci nber.
+0, 1, 1, 2, 3, 5, 8, 13 ...
+*/
+
+var fibn = fibHelper(5);
+
+console.log(fibn);
+
+function fibHelper(n) {
+  return fib(n, Array(n+1).fill(0));
+}
+
+function fib(i, memo) {
+  if (i === 0 || i === 1) return i;
+  
+  if (memo[i] === 0) {
+    memo[i] = fib(i-1, memo) + fib(i-2, memo);
+  }
+  
+  return memo[i];
+  
+}
