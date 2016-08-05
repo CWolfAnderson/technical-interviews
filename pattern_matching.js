52e6e1cde16f0084fb1a6ac92f42a285abeb58f5
Christoph Anderson <cwolfanderson@gmail.com>
2016-08-04 21:13:20 -0700

    add log message and clarifications

--- pattern_matching.js
+++ pattern_matching.js
@@ -1,23 +1,25 @@
 /*
- Given a Pattern and a dictionary, print out all the strings   that match the pattern. where a character in the pattern is mapped uniquely to a character in the dictionary ( this is what i was given first). e.g 1. ("abc" , <"cdf", "too", "hgfdt" ,"paa">) -> output = "cdf" 2. ("acc" , <"cdf", "too", "hgfdt" ,"paa">) -> output = "too", "paa" 
+ Given a Pattern and a dictionary, print out all the strings   that match the pattern. where a character in the pattern is mapped uniquely to a character in the dictionary ( this is what i was given first). e.g 1. ('abc' , <'cdf', 'too', 'hgfdt' ,'paa'>) -> output = 'cdf' 2. ('acc' , <'cdf', 'too', 'hgfdt' ,'paa'>) -> output = 'too', 'paa' 
 */
 
 /*
 Each array index refers to each occurrance of that letter
-"abc" -> [0, 1, 2]
-"cdf" -> [0, 1, 2]
-"too" -> [0, 1, 1]
-"hgfdt" -> too long
-"paa" -> [0, 1, 1]
+'abc' -> [0, 1, 2]
+'cdf' -> [0, 1, 2]
+'too' -> [0, 1, 1]
+'hgfdt' -> too long
+'paa' -> [0, 1, 1]
 */
 
-// let pattern = "abc";
-// let dictionary = ["cdf", "too", "hgfdt" ,"paa"];
+// let pattern = 'abc';
+// let dictionary = ['cdf', 'too', 'hgfdt' ,'paa'];
 // let result = matchPattern(pattern, dictionary);
 // console.log(result);
 
-let pattern = "acc";
-let dictionary = ["cdf", "too", "hgfdt" ,"paa"];
+'use strict';
+
+let pattern = 'acc';
+let dictionary = ['cdf', 'too', 'hgfdt' ,'paa'];
 let result = matchPattern(pattern, dictionary);
 console.log(result);
 
@@ -33,6 +35,8 @@ function matchPattern(pat, dict) {
   
   // map pattern to pattern
   let pattern = patternMap(pat);
+  
+  console.log(`pattern: ${pattern}`);
   
   let toReturn = [];
   patternArr.forEach(function(arr, i) {
@@ -60,7 +64,7 @@ function patternMap(word, i) {
     
   }
   
-  // console.log(word + " -> " + wordArr);
+  // console.log(word + ' -> ' + wordArr);
   
   return wordArr;
 }
